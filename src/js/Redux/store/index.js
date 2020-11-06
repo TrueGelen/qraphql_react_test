3d0c00b13c9a576ea36ca1d489b496005cc9df7a
TrueGelen <54402826+TrueGelen@users.noreply.github.com>
2020-11-06 18:26:30 +0300

    fixed rudux devtools ext

--- src/js/Redux/store/index.js
+++ src/js/Redux/store/index.js
@@ -8,8 +8,8 @@ const store = createStore(
   compose(
     applyMiddleware(
 
-    ),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )/* ,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() */
   )
 )
 
